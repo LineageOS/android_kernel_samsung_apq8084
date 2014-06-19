f49bf1b9f9bd15d6979ba57dd8e5ca747e9cbb24
Ivan Tiyanov <ivant@codeaurora.org>
2014-06-19 11:40:03 +0300

    platform: msm: sensor: Fix out of bounds and null pointer.
    This change fixes out of bounds and null pointer
    dereferences in sensor drivers.
    
    Change-Id: I5f8974d3435c2770f6e91809c153a98aa38f52a7
    Signed-off-by: Ivan Tiyanov <ivant@codeaurora.org>
    Signed-off-by: Lakshmi Narayana Kalavala <lkalaval@codeaurora.org>
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_dt_util.c
+++ drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_dt_util.c
@@ -1856,6 +1856,8 @@ power_up_failed:
 				0);
 			break;
 		case SENSOR_GPIO:
+			if (!ctrl->gpio_conf->gpio_num_info)
+				continue;
 			if (!ctrl->gpio_conf->gpio_num_info->valid
 				[power_setting->seq_val]) {
 				pr_warn("%s : Warning!! Check GPIO index %d\n",
