57b7be5a97ab425388368766e9679d91de43106d
Ravi Vembu <a18394@motorola.com>
2014-07-03 23:52:55 -0500

    usb: dwc3: Fix Unbalanced irq wake for hs_phy_irq
    Detection of TA chargers causes a SDP to DCP transition. Since the
    hs_phy_irq is wakeup disabled for DCP  in the suspend path, the
    SDP to DCP transition would cause an unbalanced irq wake access as
    the hs_phy_irq was never wake enabled in the resume path. Avoid
    this by checking state before attempting to enable/disable irq wake.
    
    Signed-off-by: Ravi Vembu <a18394@motorola.com>
    
    Change-Id: Ic5eba396c424d789fe17299c2bc7edc640aeb55c
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -215,6 +215,7 @@ struct dwc3_msm {
 
 	unsigned int		irq_to_affin;
 	struct notifier_block	dwc3_cpu_notifier;
+	atomic_t		hs_phy_irq_wake;
 };
 
 #define USB_HSPHY_3P3_VOL_MIN		3050000 /* uV */
@@ -1559,8 +1560,10 @@ static int dwc3_msm_suspend(struct dwc3_msm *mdwc)
 	if (mdwc->hs_phy_irq) {
 		enable_irq(mdwc->hs_phy_irq);
 		/* with DCP we dont require wakeup using HS_PHY_IRQ */
-		if (dcp)
+		if (dcp && atomic_read(&mdwc->hs_phy_irq_wake)) {
+			atomic_set(&mdwc->hs_phy_irq_wake, 0);
 			disable_irq_wake(mdwc->hs_phy_irq);
+		}
 	}
 
 	return 0;
@@ -1674,8 +1677,10 @@ static int dwc3_msm_resume(struct dwc3_msm *mdwc)
 		mdwc->lpm_irq_seen = false;
 	}
 	/* it must DCP disconnect, re-enable HS_PHY wakeup IRQ */
-	if (mdwc->hs_phy_irq && dcp)
+	if (mdwc->hs_phy_irq && dcp && !atomic_read(&mdwc->hs_phy_irq_wake)) {
+		atomic_set(&mdwc->hs_phy_irq_wake, 1);
 		enable_irq_wake(mdwc->hs_phy_irq);
+	}
 
 	dev_info(mdwc->dev, "DWC3 exited from low power mode\n");
 
@@ -2616,6 +2621,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 			goto disable_ref_clk;
 		}
 		enable_irq_wake(mdwc->hs_phy_irq);
+		atomic_set(&mdwc->hs_phy_irq_wake, 1);
 	}
 
 	if (mdwc->ext_xceiv.otg_capability) {
@@ -3014,7 +3020,10 @@ static int dwc3_msm_remove(struct platform_device *pdev)
 	if (!IS_ERR_OR_NULL(mdwc->vbus_otg))
 		regulator_disable(mdwc->vbus_otg);
 
-	disable_irq_wake(mdwc->hs_phy_irq);
+	if (atomic_read(&mdwc->hs_phy_irq_wake)) {
+		atomic_set(&mdwc->hs_phy_irq_wake, 0);
+		disable_irq_wake(mdwc->hs_phy_irq);
+	}
 
 	clk_disable_unprepare(mdwc->utmi_clk);
 	clk_disable_unprepare(mdwc->core_clk);
