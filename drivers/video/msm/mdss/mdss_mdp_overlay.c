a506d59bf49b373d7e69a5232ad63f9385c41326
Jeevan Shriram <jshriram@codeaurora.org>
2014-09-18 10:42:18 -0700

    msm: mdss: do register programming only when parameters have changed
    Add a check to compare whether parameters have changed from previously
    set parameters and only then do reprogramming of structure. This can
    save some time by not needing to reprogram the hardware and makes it
    easier to debug by showing logs only when there are some changes in
    parameters.
    
    Change-Id: Ic26db2906358594fa6475010df6d6e05bc0cb4ad
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -636,15 +636,6 @@ int mdss_mdp_overlay_pipe_setup(struct msm_fb_data_type *mfd,
 	if (ret)
 		return ret;
 
-	pipe = mdss_mdp_get_staged_pipe(mdp5_data->ctl, mixer_mux,
-		req->z_order, left_blend_pipe != NULL);
-	if (pipe && pipe->ndx != req->id) {
-		pr_debug("replacing pnum=%d at stage=%d mux=%d id:0x%x %s\n",
-			pipe->num, req->z_order, mixer_mux, req->id,
-			left_blend_pipe ? "right blend" : "left blend");
-		mdss_mdp_mixer_pipe_unstage(pipe, pipe->mixer_left);
-	}
-
 	mixer = mdss_mdp_mixer_get(mdp5_data->ctl, mixer_mux);
 	if (!mixer) {
 		pr_err("unable to get mixer\n");
@@ -761,6 +752,12 @@ int mdss_mdp_overlay_pipe_setup(struct msm_fb_data_type *mfd,
 		pipe->is_right_blend = false;
 	}
 
+	req->priority = pipe->priority;
+	if (!memcmp(req, &pipe->req_data, sizeof(*req))) {
+		pr_debug("skipping pipe_reconfiguration\n");
+		goto skip_reconfigure;
+	}
+
 	pipe->flags = req->flags;
 	if (bwc_enabled  &&  !mdp5_data->mdata->has_bwc) {
 		pr_err("BWC is not supported in MDP version %x\n",
@@ -842,10 +839,6 @@ int mdss_mdp_overlay_pipe_setup(struct msm_fb_data_type *mfd,
 		pipe->overfetch_disable = 0;
 	}
 	pipe->bg_color = req->bg_color;
-
-	req->id = pipe->ndx;
-	req->priority = pipe->priority;
-	pipe->req_data = *req;
 
 	mdss_mdp_pipe_sspp_term(pipe);
 
@@ -938,16 +931,19 @@ int mdss_mdp_overlay_pipe_setup(struct msm_fb_data_type *mfd,
 		goto exit_fail;
 	}
 
-	pipe->params_changed++;
+	req->id = pipe->ndx;
 
 	req->vert_deci = pipe->vert_deci;
 
+	pipe->req_data = *req;
+
+	pipe->params_changed++;
+skip_reconfigure:
 	*ppipe = pipe;
 
 	mdss_mdp_pipe_unmap(pipe);
 
 	return ret;
-
 exit_fail:
 	mdss_mdp_pipe_unmap(pipe);
 
