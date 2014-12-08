9f47ba7805015c121fb4acd4977c8f6d2dada8ca
sbrissen <sbrissen@hotmail.com>
2014-12-08 00:27:21 -0500

    cypress-touchkey: Add key disabler.
    Change-Id: I16fb039c32f63938e4aece5faa5e2a46bed411d6

--- drivers/input/keyboard/cypress_touchkey_t/cypress-touchkey.c
+++ drivers/input/keyboard/cypress_touchkey_t/cypress-touchkey.c
@@ -621,6 +621,10 @@ static irqreturn_t cypress_touchkey_interrupt(int irq, void *dev_id)
 	int ret;
 	int i;
 
+	if (!atomic_read(&info->keypad_enable)) {
+			goto out;
+	}
+
 	ret = gpio_get_value(info->pdata->gpio_int);
 	if (ret) {
 		dev_info(&info->client->dev,
@@ -1486,6 +1490,38 @@ static ssize_t cypress_touchkey_read_register(struct device *dev,
 	return size;
 }
 
+static ssize_t sec_keypad_enable_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct cypress_touchkey_info *info = dev_get_drvdata(dev);
+
+	return sprintf(buf, "%d\n", atomic_read(&info->keypad_enable));
+}
+
+static ssize_t sec_keypad_enable_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct cypress_touchkey_info *info = dev_get_drvdata(dev);
+	int i;
+
+	unsigned int val = 0;
+	sscanf(buf, "%d", &val);
+	val = (val == 0 ? 0 : 1);
+	atomic_set(&info->keypad_enable, val);
+	if (val) {
+		for (i = 0; i < ARRAY_SIZE(info->keycode); i++)
+			set_bit(info->keycode[i], info->input_dev->keybit);
+	} else {
+		for (i = 0; i < ARRAY_SIZE(info->keycode); i++)
+			clear_bit(info->keycode[i], info->input_dev->keybit);
+	}
+	input_sync(info->input_dev);
+
+	return count;
+}
+
+static DEVICE_ATTR(keypad_enable, S_IRUGO|S_IWUSR, sec_keypad_enable_show,
+	      sec_keypad_enable_store);
 static DEVICE_ATTR(touchkey_firm_update_status, S_IRUGO | S_IWUSR | S_IWGRP,
 		cypress_touchkey_firm_status_show, NULL);
 static DEVICE_ATTR(touchkey_firm_version_panel, S_IRUGO,
@@ -1554,6 +1590,7 @@ static DEVICE_ATTR(read_reg, S_IRUGO | S_IWUSR | S_IWGRP, NULL,
 		cypress_touchkey_read_register);
 
 static struct attribute *touchkey_attributes[] = {
+	&dev_attr_keypad_enable.attr,
 	&dev_attr_touchkey_firm_update_status.attr,
 	&dev_attr_touchkey_firm_version_panel.attr,
 	&dev_attr_touchkey_firm_version_phone.attr,
@@ -2003,6 +2040,8 @@ static int cypress_touchkey_probe(struct i2c_client *client,
 	set_bit(EV_KEY, input_dev->evbit);
 	set_bit(EV_LED, input_dev->evbit);
 	set_bit(LED_MISC, input_dev->ledbit);
+
+	atomic_set(&info->keypad_enable, 1);
 
 	wake_lock_init(&info->fw_wakelock, WAKE_LOCK_SUSPEND, "cypress_touchkey");
 
