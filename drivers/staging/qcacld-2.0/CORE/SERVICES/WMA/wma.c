2275f7924d88ef13c791f3320aff7b0195395776
Sreelakshmi Konamki <c_skonam@qti.qualcomm.com>
2015-09-10 19:20:52 +0530

    qcacld-2.0: Add DS param IE in probe req frame
    Currently DS param IE is not present in probe req frames.
    Set bit11 in scan_ctrl_flags to add DS param IE in probe req.
    
    Change-Id: I57f79bd6dc5bd452987cb7d09cafa425ea2bbff4
    CRs-Fixed: 905421

--- drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.c
+++ drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.c
@@ -7694,6 +7694,9 @@ VOS_STATUS wma_get_buf_start_scan_cmd(tp_wma_handle wma_handle,
 	/* Large timeout value for full scan cycle, 30 seconds */
 	cmd->max_scan_time = WMA_HW_DEF_SCAN_MAX_DURATION;
 
+	/* add DS param IE in probe req frame */
+	cmd->scan_ctrl_flags |= WMI_SCAN_ADD_DS_IE_IN_PROBE_REQ;
+
 	/* do not add OFDM rates in 11B mode */
 	if (scan_req->dot11mode != WNI_CFG_DOT11_MODE_11B)
 		cmd->scan_ctrl_flags |= WMI_SCAN_ADD_OFDM_RATES;
@@ -9173,7 +9176,8 @@ v_VOID_t wma_roam_scan_fill_scan_params(tp_wma_handle wma_handle,
 
     vos_mem_zero(scan_params, sizeof(wmi_start_scan_cmd_fixed_param));
     scan_params->scan_ctrl_flags = WMI_SCAN_ADD_CCK_RATES |
-                WMI_SCAN_ADD_OFDM_RATES;
+                                   WMI_SCAN_ADD_OFDM_RATES |
+                                   WMI_SCAN_ADD_DS_IE_IN_PROBE_REQ;
     if (roam_req != NULL) {
         /* Parameters updated after association is complete */
         WMA_LOGI("%s: Input parameters: NeighborScanChannelMinTime"
@@ -22923,7 +22927,8 @@ static VOS_STATUS wma_process_ll_stats_getReq
 	cmd->scan_ctrl_flags = WMI_SCAN_ADD_BCAST_PROBE_REQ |
 				WMI_SCAN_ADD_CCK_RATES |
 				WMI_SCAN_ADD_OFDM_RATES |
-				WMI_SCAN_ADD_SPOOFED_MAC_IN_PROBE_REQ;
+				WMI_SCAN_ADD_SPOOFED_MAC_IN_PROBE_REQ |
+				WMI_SCAN_ADD_DS_IE_IN_PROBE_REQ;
 	cmd->scan_priority = WMI_SCAN_PRIORITY_HIGH;
 	cmd->num_ssids = 0;
 	cmd->num_bssid = 0;
