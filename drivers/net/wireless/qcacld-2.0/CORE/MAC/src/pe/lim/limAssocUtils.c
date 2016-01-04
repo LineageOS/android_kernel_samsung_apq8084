8f468dc020d0bc72faf6b22c6354c0147d992629
Naveen Rawat <nrawat@qca.qualcomm.com>
2016-01-04 12:33:49 -0800

    qcacld-2.0: Proper mem allocation for lim, pre auth timers
    Fix mem alloc for pre auth timer and add error handling for limInitMlm.
    
    Currently number of preauth timer is taken as cfg param and memory is
    allocated in bulk. In case the memory allocation fails, no error is
    returned from limInitMlm, which causes undefined behavior. Also preauth
    timers do not need contiguous memory allocation, so allocate an array of
    pointers instead of single chunk for pre auth timers.
    
    Change-Id: I92677aa9bb7970034630f4c6487fa9d196ccfb4c
    CRs-Fixed: 955557

--- drivers/net/wireless/qcacld-2.0/CORE/MAC/src/pe/lim/limAssocUtils.c
+++ drivers/net/wireless/qcacld-2.0/CORE/MAC/src/pe/lim/limAssocUtils.c
@@ -4666,7 +4666,7 @@ void limInitPreAuthTimerTable(tpAniSirGlobal pMac, tpLimPreAuthTable pPreAuthTim
 {
     tANI_U32 cfgValue;
     tANI_U32 authNodeIdx;
-    tpLimPreAuthNode pAuthNode = pPreAuthTimerTable->pTable;
+    tLimPreAuthNode **pAuthNode = pPreAuthTimerTable->pTable;
 
     // Get AUTH_RSP Timers value
 
@@ -4683,9 +4683,9 @@ void limInitPreAuthTimerTable(tpAniSirGlobal pMac, tpLimPreAuthTable pPreAuthTim
     }
 
     cfgValue = SYS_MS_TO_TICKS(cfgValue);
-    for(authNodeIdx=0; authNodeIdx<pPreAuthTimerTable->numEntry; authNodeIdx++, pAuthNode++)
+    for(authNodeIdx=0; authNodeIdx<pPreAuthTimerTable->numEntry; authNodeIdx++)
     {
-        if (tx_timer_create(&pAuthNode->timer,
+        if (tx_timer_create(&(pAuthNode[authNodeIdx]->timer),
                         "AUTH RESPONSE TIMEOUT",
                         limAuthResponseTimerHandler,
                         authNodeIdx,
@@ -4697,10 +4697,9 @@ void limInitPreAuthTimerTable(tpAniSirGlobal pMac, tpLimPreAuthTable pPreAuthTim
             limLog(pMac, LOGP, FL("Cannot create Auth Rsp timer of Index :%d."), authNodeIdx);
             return;
         }
-        pAuthNode->authNodeIdx = (tANI_U8)authNodeIdx;
-        pAuthNode->fFree = 1;
+        pAuthNode[authNodeIdx]->authNodeIdx = (tANI_U8)authNodeIdx;
+        pAuthNode[authNodeIdx]->fFree = 1;
     }
-
 }
 
 /** -------------------------------------------------------------
@@ -4713,13 +4712,11 @@ void limInitPreAuthTimerTable(tpAniSirGlobal pMac, tpLimPreAuthTable pPreAuthTim
 tLimPreAuthNode * limAcquireFreePreAuthNode(tpAniSirGlobal pMac, tpLimPreAuthTable pPreAuthTimerTable)
 {
     tANI_U32 i;
-    tLimPreAuthNode *pTempNode = pPreAuthTimerTable->pTable;
-    for (i=0; i<pPreAuthTimerTable->numEntry; i++,pTempNode++)
-    {
-        if (pTempNode->fFree == 1)
-        {
-            pTempNode->fFree = 0;
-            return pTempNode;
+    tLimPreAuthNode **pTempNode = pPreAuthTimerTable->pTable;
+    for (i=0; i < pPreAuthTimerTable->numEntry; i++) {
+        if (pTempNode[i]->fFree == 1) {
+            pTempNode[i]->fFree = 0;
+            return pTempNode[i];
         }
     }
 
@@ -4744,7 +4741,7 @@ tLimPreAuthNode * limGetPreAuthNodeFromIndex(tpAniSirGlobal pMac,
         return NULL;
     }
 
-    return pAuthTable->pTable + authNodeIdx;
+    return pAuthTable->pTable[authNodeIdx];
 }
 
 /* Util API to check if the channels supported by STA is within range */
