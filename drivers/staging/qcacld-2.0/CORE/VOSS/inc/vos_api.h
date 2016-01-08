80262fd1e189f2b00e2515438b38b0badf7836f2
Gupta, Kapil <kapgupta@qti.qualcomm.com>
2016-01-08 15:30:19 +0530

    qcacld-2.0: Changes to detect thread stuck issues in wlan driver
    prima to qcacld-2.0 propagation
    
    This fix contains the necessary changes to detect thread stuck
    issues in driver. The changes are to post message to MC thread
    for every few seconds, which elicits a response from this thread.
    A loss of response for consecutive three times is regarded as
    thread stuck.
    
    Change-Id: I3184db4dd7472f0c65dd607af7c197c52139cdb0
    CRs-Fixed: 862223

--- drivers/staging/qcacld-2.0/CORE/VOSS/inc/vos_api.h
+++ drivers/staging/qcacld-2.0/CORE/VOSS/inc/vos_api.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2016 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -362,4 +362,5 @@ VOS_STATUS vos_flush_logs(uint32_t is_fatal,
 		uint32_t indicator,
 		uint32_t reason_code);
 void vos_logging_set_fw_flush_complete(void);
+void vos_probe_threads(void);
 #endif // if !defined __VOS_API_H
