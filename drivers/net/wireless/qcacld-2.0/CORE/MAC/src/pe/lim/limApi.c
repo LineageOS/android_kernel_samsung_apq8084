ebb9d5d4c09c8453f9abba9d61387eb2ce44eca4
Naveen Rawat <nrawat@qca.qualcomm.com>
2016-01-11 12:55:40 -0800

    qcacld-2.0: Fix memory leak for lim preauth timers at driver unload
    Fix memory leak at driver unload by freeing memory for lim preauth
    timers.
    
    Change-Id: I7be177e6c475a8f8fdc4cc7d2744e34a4bf5dbf5
    CRs-Fixed: 960617

--- drivers/net/wireless/qcacld-2.0/CORE/MAC/src/pe/lim/limApi.c
+++ drivers/net/wireless/qcacld-2.0/CORE/MAC/src/pe/lim/limApi.c
@@ -734,6 +734,7 @@ limCleanup(tpAniSirGlobal pMac)
 {
 //Before destroying the list making sure all the nodes have been deleted.
 //Which should be the normal case, but a memory leak has been reported.
+    uint8_t i;
 
     tpLimMgmtFrameRegistration pLimMgmtRegistration = NULL;
 
@@ -754,6 +755,8 @@ limCleanup(tpAniSirGlobal pMac)
     // free up preAuth table
     if (pMac->lim.gLimPreAuthTimerTable.pTable != NULL)
     {
+        for (i = 0; i < pMac->lim.gLimPreAuthTimerTable.numEntry; i++)
+            vos_mem_free(pMac->lim.gLimPreAuthTimerTable.pTable[i]);
         vos_mem_free(pMac->lim.gLimPreAuthTimerTable.pTable);
         pMac->lim.gLimPreAuthTimerTable.pTable = NULL;
         pMac->lim.gLimPreAuthTimerTable.numEntry = 0;
