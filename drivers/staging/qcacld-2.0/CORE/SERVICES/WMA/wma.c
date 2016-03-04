ee426c7cf9f7ed91a83803b2a4b6da2b86969ac6
Yingying Tang <yintang@qti.qualcomm.com>
2016-03-04 13:44:42 +0800

    qcacld-2.0: Avoid memory leak in wma_tdls_event_handler()
    Add vos_mem_free() in wma_tdls_event_handler() to avoid memory leak
    in the error case.
    
    Change-Id: I845d5ee9f42569efddb89e29bfd158de5ab7f0d9
    CRs-Fixed: 979671

--- drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.c
+++ drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.c
@@ -4951,6 +4951,7 @@ static int wma_tdls_event_handler(void *handle, u_int8_t *event, u_int32_t len)
 	 tdls_event->messageType = WDA_TDLS_PEER_DISCONNECTED;
 	 break;
 	default:
+	 vos_mem_free(tdls_event);
 	 WMA_LOGE("%s: Discarding unknown tdls event(%d) from target",
 	          __func__, peer_event->peer_status);
 	 return -1;
@@ -4979,6 +4980,7 @@ static int wma_tdls_event_handler(void *handle, u_int8_t *event, u_int32_t len)
 		tdls_event->peer_reason = eWNI_TDLS_TEARDOWN_REASON_NO_RESPONSE;
 		break;
 	default:
+		vos_mem_free(tdls_event);
 		WMA_LOGE("%s: unknown reason(%d) in tdls event(%d) from target",
 		         __func__, peer_event->peer_reason, peer_event->peer_status);
 		return -1;
