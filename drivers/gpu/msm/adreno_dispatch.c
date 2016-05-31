e0dc82a4cb36490067899c7e5d9fecce69897ebe
Jordan Crouse <jcrouse@codeaurora.org>
2016-05-31 17:24:30 +0000

    msm: kgsl: Remove event worker
    Scheduling issues were occurring with the GPU event worker after
    b7be807 (msm: kgsl: Unbind the kgsl-event workqueue) was merged.
    In certain conditions, it seems that the kgsl-event workqueue
    was conflicting with the KGSL worker and slowing it down.
    
    It turns out that everywhere we schedule the event worker
    and the dispatcher worker at the same time.  Since the worker
    is singlethread, the event worker and the dispatcher run
    synchronously anyway, so it makes sense to run the event processor
    from within the dispatcher and save the extra schedule.
    
    Change-Id: Ic0dedbad67eb04d41afb6add4477f146dfff9784
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    [neo: Make workqueue high-priority.]

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -1536,6 +1536,8 @@ static void adreno_dispatcher_work(struct kthread_work *work)
 		break;
 	}
 
+	kgsl_process_event_groups(device);
+
 	/*
 	 * Call the dispatcher fault routine here so the fault bit gets cleared
 	 * when no commands are in dispatcher but fault bit is set. This can
@@ -1543,13 +1545,6 @@ static void adreno_dispatcher_work(struct kthread_work *work)
 	 */
 	if (!fault_handled && dispatcher_do_fault(device))
 		goto done;
-
-	/*
-	 * If inflight went to 0, queue back up the event processor to catch
-	 * stragglers
-	 */
-	if (dispatcher->inflight == 0 && count)
-		queue_work(device->work_queue, &device->event_work);
 
 	/* Dispatch new commands if we have the room */
 	if (dispatcher->inflight < _dispatcher_inflight)
