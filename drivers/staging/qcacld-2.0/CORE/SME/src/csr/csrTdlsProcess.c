782003edb30e3f07cb3e2915a8150cef2ee45f36
Mahesh A Saptasagar <c_msapta@qti.qualcomm.com>
2016-07-01 15:41:16 +0530

    qcacld-2.0: Fix for potential buffer overflow and null pointer references
    prima to qcacld-2.0 propagation
    
    Static analyser is reporting errors for array bound
    checking and null pointer references.
    To resolve this
    1. Check for condition array index shouldn't exceed
       WNI_CFG_VALID_CHANNEL_LIST_LEN before accessing
       ChannelList array.
    2. Check for NULL condition wherever necessary.
    
    Change-Id: Idd0a23a8180dddabfdd353c0861899411aecfa16
    CRs-Fixed: 534624

--- drivers/staging/qcacld-2.0/CORE/SME/src/csr/csrTdlsProcess.c
+++ drivers/staging/qcacld-2.0/CORE/SME/src/csr/csrTdlsProcess.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2016 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -147,7 +147,7 @@ eHalStatus csrTdlsSendMgmtReq(tHalHandle hHal, tANI_U8 sessionId, tCsrTdlsSendMg
 }
 
 /*
- * TDLS request API, called from HDD to add a TDLS peer
+ * TDLS request API, called from HDD to modify an existing TDLS peer
  */
 eHalStatus csrTdlsChangePeerSta(tHalHandle hHal, tANI_U8 sessionId, tSirMacAddr peerMac,
                                 tCsrStaParams *pstaParams)
@@ -156,6 +156,8 @@ eHalStatus csrTdlsChangePeerSta(tHalHandle hHal, tANI_U8 sessionId, tSirMacAddr 
     tSmeCmd *tdlsAddStaCmd ;
     eHalStatus status = eHAL_STATUS_FAILURE ;
 
+    if (NULL == pstaParams)
+        return status;
     //If connected and in Infra. Only then allow this
     if (CSR_IS_SESSION_VALID( pMac, sessionId ) &&
         csrIsConnStateConnectedInfra( pMac, sessionId ) &&
