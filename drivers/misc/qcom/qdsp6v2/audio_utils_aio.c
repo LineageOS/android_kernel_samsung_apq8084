bfeadbe3f7604b064ef4683b601a7954e11a52c6
Haynes Mathew George <hgeorge@codeaurora.org>
2016-10-05 15:41:49 -0700

    ASoC: msm: Align lock read/write when add/free audio ion memory to shamu
    Change-Id: I76920fc1aa1cb83a42d17ede4b4c5a0fc597327b
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- drivers/misc/qcom/qdsp6v2/audio_utils_aio.c
+++ drivers/misc/qcom/qdsp6v2/audio_utils_aio.c
@@ -577,9 +577,9 @@ int audio_aio_release(struct inode *inode, struct file *file)
 	wake_up(&audio->event_wait);
 	audio_aio_reset_event_queue(audio);
 	q6asm_audio_client_free(audio->ac);
-	mutex_unlock(&audio->write_lock);
-	mutex_unlock(&audio->read_lock);
 	mutex_unlock(&audio->lock);
+	mutex_unlock(&audio->read_lock);
+	mutex_unlock(&audio->write_lock);
 	mutex_destroy(&audio->lock);
 	mutex_destroy(&audio->read_lock);
 	mutex_destroy(&audio->write_lock);
@@ -1350,32 +1350,30 @@ long audio_aio_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 		struct msm_audio_ion_info info;
 		pr_debug("%s[%p]:AUDIO_REGISTER_ION\n", __func__, audio);
 		mutex_lock(&audio->lock);
+		mutex_lock(&audio->read_lock);
+		mutex_lock(&audio->write_lock);
 		if (copy_from_user(&info, (void *)arg, sizeof(info)))
 			rc = -EFAULT;
-		else {
-			mutex_lock(&audio->read_lock);
-			mutex_lock(&audio->write_lock);
+		else
 			rc = audio_aio_ion_add(audio, &info);
-			mutex_unlock(&audio->write_lock);
-			mutex_unlock(&audio->read_lock);
-		}
 		mutex_unlock(&audio->lock);
+		mutex_unlock(&audio->read_lock);
+		mutex_unlock(&audio->write_lock);
 		break;
 	}
 	case AUDIO_DEREGISTER_ION: {
 		struct msm_audio_ion_info info;
 		mutex_lock(&audio->lock);
+		mutex_lock(&audio->read_lock);
+		mutex_lock(&audio->write_lock);
 		pr_debug("%s[%p]:AUDIO_DEREGISTER_ION\n", __func__, audio);
 		if (copy_from_user(&info, (void *)arg, sizeof(info)))
 			rc = -EFAULT;
-		else {
-			mutex_lock(&audio->read_lock);
-			mutex_lock(&audio->write_lock);
+		else
 			rc = audio_aio_ion_remove(audio, &info);
-			mutex_unlock(&audio->write_lock);
-			mutex_unlock(&audio->read_lock);
-		}
 		mutex_unlock(&audio->lock);
+		mutex_unlock(&audio->read_lock);
+		mutex_unlock(&audio->write_lock);
 		break;
 	}
 	case AUDIO_GET_STREAM_CONFIG: {
