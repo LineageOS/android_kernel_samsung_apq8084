6e936aea4627ea1c69636a8b22cacaea3a8282b9
Juha Inkinen <inkinen@gmail.com>
2017-01-10 12:14:54 +0200

    Update QCOM MHI driver
    Updated Qualcomm MHI (modem-host interface) driver
    from N910FXXS1DPK3. Seems to fix many MHI/rmnet errors seen in
    kernel dmesg. These were probably the cause of data drops.
    
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- drivers/platform/msm/mhi/mhi_ring_ops.c
+++ drivers/platform/msm/mhi/mhi_ring_ops.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -13,8 +13,17 @@
 #include "mhi_sys.h"
 #include "mhi.h"
 
-static enum MHI_STATUS add_element(struct mhi_ring *ring, void **rp,
-			void **wp, void **assigned_addr)
+inline MHI_STATUS ctxt_add_element(mhi_ring *ring, void **assigned_addr)
+{
+	return add_element(ring, &ring->rp, &ring->wp, assigned_addr);
+}
+inline MHI_STATUS ctxt_del_element(mhi_ring *ring, void **assigned_addr)
+{
+	return delete_element(ring, &ring->rp, &ring->wp, assigned_addr);
+}
+
+MHI_STATUS add_element(mhi_ring *ring, void * volatile *rp,
+			void * volatile *wp, void **assigned_addr)
 {
 	uintptr_t d_wp = 0, d_rp = 0, ring_size = 0;
 
@@ -49,29 +58,16 @@ static enum MHI_STATUS add_element(struct mhi_ring *ring, void **rp,
 						(uintptr_t)ring->base);
 	return MHI_STATUS_SUCCESS;
 }
-
-inline enum MHI_STATUS ctxt_add_element(struct mhi_ring *ring,
-						void **assigned_addr)
-{
-	return add_element(ring, &ring->rp, &ring->wp, assigned_addr);
-}
-inline enum MHI_STATUS ctxt_del_element(struct mhi_ring *ring,
-						void **assigned_addr)
-{
-	return delete_element(ring, &ring->rp, &ring->wp, assigned_addr);
-}
-
 /**
- * delete_element - Moves the read pointer of the transfer ring to
- * the next element of the transfer ring,
- *
- * ring location of local ring data structure
- * @rp ring read pointer
- * @wp ring write pointer
- * @assigned_addr location of the element just deleted
- */
-enum MHI_STATUS delete_element(struct mhi_ring *ring, void **rp,
-			void **wp, void **assigned_addr)
+*@brief Moves the read pointer of the transfer ring to
+*the next element of the transfer ring,
+*	as the element has been consumed by the mhi client.
+*
+*@param[in ] ring location of local register set for the ring
+*@param[out] assigned_addr address returned to the calee with element
+*/
+MHI_STATUS delete_element(mhi_ring *ring, void * volatile *rp,
+			void * volatile *wp, void **assigned_addr)
 {
 	uintptr_t d_wp = 0, d_rp = 0, ring_size = 0;
 
@@ -107,33 +103,31 @@ enum MHI_STATUS delete_element(struct mhi_ring *ring, void **rp,
 	return MHI_STATUS_SUCCESS;
 }
 
-int mhi_get_free_desc(struct mhi_client_handle *client_handle)
+/**
+ * @brief Returns the number of free ring elements.
+ *
+ * @param[in ]	ring	location of local register set for the ring
+ * @param[out]	nr_of_avail_trbs returns the number of free elements
+ */
+int get_free_trbs(mhi_client_handle *client_handle)
 {
 	u32 chan = client_handle->chan;
-	struct mhi_device_ctxt *ctxt = client_handle->mhi_dev_ctxt;
-	if (NULL == client_handle || MHI_HANDLE_MAGIC != client_handle->magic)
-		return -EINVAL;
-	return get_nr_avail_ring_elements(&ctxt->mhi_local_chan_ctxt[chan]);
+	u32 available_ring_els = 0;
+	mhi_device_ctxt *ctxt = client_handle->mhi_dev_ctxt;
+	available_ring_els =
+		get_nr_avail_ring_elements(&ctxt->mhi_local_chan_ctxt[chan]);
+	return	available_ring_els;
 }
-EXPORT_SYMBOL(mhi_get_free_desc);
-
-int get_nr_avail_ring_elements(struct mhi_ring *ring)
+int get_nr_avail_ring_elements(mhi_ring *ring)
 {
 	u32 nr_el = 0;
 	uintptr_t ring_size = 0;
-	enum MHI_STATUS ret_val = MHI_STATUS_SUCCESS;
 	ring_size = ring->len / ring->el_size;
-	ret_val = get_nr_enclosed_el(ring, ring->rp, ring->wp, &nr_el);
-	if (ret_val != MHI_STATUS_SUCCESS) {
-		mhi_log(MHI_MSG_ERROR,
-			"Failed to get enclosed el ret %d.\n", ret_val);
-		return 0;
-	}
+	get_nr_enclosed_el(ring, ring->rp, ring->wp, &nr_el);
 	return ring_size - nr_el - 1;
 }
 
-enum MHI_STATUS get_nr_enclosed_el(struct mhi_ring *ring, void *rp,
-						void *wp, u32 *nr_el)
+MHI_STATUS get_nr_enclosed_el(mhi_ring *ring, void *rp, void *wp, u32 *nr_el)
 {
 	uintptr_t index_rp = 0;
 	uintptr_t index_wp = 0;
@@ -144,12 +138,12 @@ enum MHI_STATUS get_nr_enclosed_el(struct mhi_ring *ring, void *rp,
 		return MHI_STATUS_ERROR;
 	}
 	if (MHI_STATUS_SUCCESS != get_element_index(ring, rp, &index_rp)) {
-		mhi_log(MHI_MSG_CRITICAL, "Bad element index rp 0x%p.\n", rp);
+		mhi_log(MHI_MSG_CRITICAL, "Bad element index.\n");
 		return MHI_STATUS_ERROR;
 	}
 
 	if (MHI_STATUS_SUCCESS != get_element_index(ring, wp, &index_wp)) {
-		mhi_log(MHI_MSG_CRITICAL, "Bad element index wp 0x%p.\n", wp);
+		mhi_log(MHI_MSG_CRITICAL, "Bad element index.\n");
 		return MHI_STATUS_ERROR;
 	}
 	ring_size = ring->len / ring->el_size;
@@ -163,8 +157,7 @@ enum MHI_STATUS get_nr_enclosed_el(struct mhi_ring *ring, void *rp,
 	return MHI_STATUS_SUCCESS;
 }
 
-enum MHI_STATUS get_element_index(struct mhi_ring *ring,
-				void *address, uintptr_t *index)
+MHI_STATUS get_element_index(mhi_ring *ring, void *address, uintptr_t *index)
 {
 	if (MHI_STATUS_SUCCESS != validate_ring_el_addr(ring,
 							(uintptr_t)address))
@@ -173,8 +166,7 @@ enum MHI_STATUS get_element_index(struct mhi_ring *ring,
 	return MHI_STATUS_SUCCESS;
 }
 
-enum MHI_STATUS get_element_addr(struct mhi_ring *ring,
-				uintptr_t index, void **address)
+MHI_STATUS get_element_addr(mhi_ring *ring, uintptr_t index, void **address)
 {
 	uintptr_t ring_size = 0;
 	if (NULL == ring || NULL == address)
--- include/linux/msm_mhi.h
+++ include/linux/msm_mhi.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -13,12 +13,12 @@
 #define MSM_MHI_H
 #include <linux/types.h>
 
-struct mhi_client_handle;
+typedef struct mhi_client_handle mhi_client_handle;
 
 #define MHI_DMA_MASK       0x3FFFFFFF
 #define MHI_MAX_MTU        0xFFFF
 
-enum MHI_STATUS {
+typedef enum MHI_STATUS {
 	MHI_STATUS_SUCCESS = 0,
 	MHI_STATUS_ERROR = 1,
 	MHI_STATUS_DEV_NOT_FOUND = 2,
@@ -36,11 +36,10 @@ enum MHI_STATUS {
 	MHI_STATUS_USERSPACE_MEM_ERR = 14,
 	MHI_STATUS_BAD_HANDLE = 15,
 	MHI_STATUS_INVALID_CHAN_ERR = 16,
-	MHI_STATUS_OVERFLOW = 17,
 	MHI_STATUS_reserved = 0x80000000
-};
+} MHI_STATUS;
 
-enum MHI_CLIENT_CHANNEL {
+typedef enum MHI_CLIENT_CHANNEL {
 	MHI_CLIENT_LOOPBACK_OUT = 0,
 	MHI_CLIENT_LOOPBACK_IN = 1,
 	MHI_CLIENT_SAHARA_OUT = 2,
@@ -61,8 +60,8 @@ enum MHI_CLIENT_CHANNEL {
 	MHI_CLIENT_IP_CTRL_0_IN = 17,
 	MHI_CLIENT_IP_CTRL_1_OUT = 18,
 	MHI_CLIENT_IP_CTRL_1_IN = 19,
-	MHI_CLIENT_DCI_OUT = 20,
-	MHI_CLIENT_DCI_IN = 21,
+	MHI_CLIENT_IP_CTRL_2_OUT = 20,
+	MHI_CLIENT_IP_CTRL_2_IN = 21,
 	MHI_CLIENT_IP_CTRL_3_OUT = 22,
 	MHI_CLIENT_IP_CTRL_3_IN = 23,
 	MHI_CLIENT_IP_CTRL_4_OUT = 24,
@@ -87,164 +86,65 @@ enum MHI_CLIENT_CHANNEL {
 	MHI_CLIENT_CSVT_IN = 43,
 	MHI_CLIENT_SMCT_OUT = 44,
 	MHI_CLIENT_SMCT_IN = 45,
-	MHI_CLIENT_TEST_OUT = 60,
-	MHI_CLIENT_TEST_IN = 61,
-	MHI_CLIENT_RESERVED_1_LOWER = 62,
+#ifdef FEATURE_UKNIGHT_MHI_DEDICATE_CHANNEL
+	MHI_CLIENT_UKNIGHT_OUT = 46,
+	MHI_CLIENT_UKNIGHT_IN = 47,
+	MHI_CLIENT_RESERVED_1_LOWER = 48,
+#else
+	MHI_CLIENT_RESERVED_1_LOWER = 46,
+#endif
 	MHI_CLIENT_RESERVED_1_UPPER = 99,
 	MHI_CLIENT_IP_HW_0_OUT = 100,
 	MHI_CLIENT_IP_HW_0_IN = 101,
 	MHI_CLIENT_RESERVED_2_LOWER = 102,
 	MHI_CLIENT_RESERVED_2_UPPER = 127,
 	MHI_MAX_CHANNELS = 102
-};
+} MHI_CLIENT_CHANNEL;
 
-enum MHI_CB_REASON {
-	MHI_CB_XFER = 0x0,
+typedef enum MHI_CB_REASON {
+	MHI_CB_XFER_SUCCESS = 0x0,
+	MHI_CB_XFER_ERROR = 0x1,
 	MHI_CB_MHI_DISABLED = 0x4,
 	MHI_CB_MHI_ENABLED = 0x8,
 	MHI_CB_CHAN_RESET_COMPLETE = 0x10,
 	MHI_CB_reserved = 0x80000000,
-};
-
-enum MHI_FLAGS {
-	MHI_EOB = 0x100,
-	MHI_EOT = 0x200,
-	MHI_CHAIN = 0x1,
-	MHI_FLAGS_reserved = 0x80000000,
-};
-
-struct mhi_result {
+} MHI_CB_REASON;
+typedef struct mhi_result {
 	void *user_data;
-	dma_addr_t payload_buf;
+	void *payload_buf;
 	u32 bytes_xferd;
-	enum MHI_STATUS transaction_status;
-	enum MHI_FLAGS flags;
-};
+	MHI_STATUS transaction_status;
+} mhi_result;
 
-struct mhi_cb_info {
-	struct mhi_result *result;
-	enum MHI_CB_REASON cb_reason;
-	u32 chan;
-};
+typedef struct mhi_cb_info {
+	mhi_result *result;
+	MHI_CB_REASON cb_reason;
+} mhi_cb_info;
+typedef struct mhi_client_info_t {
+	void (*mhi_client_cb)(mhi_cb_info *);
+	u32 cb_mod;
+	u32 intmod_t;
+} mhi_client_info_t;
 
-struct mhi_client_info_t {
-	void (*mhi_client_cb)(struct mhi_cb_info *);
-};
+MHI_STATUS mhi_open_channel(mhi_client_handle **client_handle,
+		MHI_CLIENT_CHANNEL chan, s32 device_index,
+		mhi_client_info_t *info, void *user_data);
+MHI_STATUS mhi_queue_xfer(mhi_client_handle *client_handle,
+		uintptr_t buf, size_t buf_len, u32 chain, u32 eob);
+void mhi_close_channel(mhi_client_handle *client_handle);
+void mhi_mask_irq(mhi_client_handle *client_handle);
+void mhi_unmask_irq(mhi_client_handle *client_handle);
+mhi_result *mhi_poll(mhi_client_handle *client_handle);
+int get_free_trbs(mhi_client_handle *client_handle);
+int mhi_get_epid(mhi_client_handle* mhi_handle);
+MHI_STATUS mhi_reset_channel(mhi_client_handle *mhi_handle);
+void mhi_poll_inbound(mhi_client_handle *client_handle,
+			uintptr_t *buf, ssize_t *buf_size);
+MHI_STATUS mhi_client_recycle_trb(mhi_client_handle *client_handle);
+int mhi_get_max_buffers(mhi_client_handle *client_handle);
 
-/**
- * mhi_deregister_channel - de-register callbacks from MHI
- *
- * @client_handle: Handle populated by MHI, opaque to client
- *
- * @Return MHI_STATUS
- */
-enum MHI_STATUS mhi_deregister_channel(struct mhi_client_handle *client_handle);
+int mhi_uci_init(void);
+int rmnet_mhi_init(void);
+int mhi_set_lpm(mhi_client_handle *client_handle, int enable_lpm);
 
-/**
- * mhi_register_channel - Client must call this function to obtain a handle for
- *			  any MHI operations
- *
- *  @client_handle:  Handle populated by MHI, opaque to client
- *  @chan:           Channel provided by client to which the handle
- *                   maps to.
- *  @device_index:   MHI device for which client wishes to register, if
- *                   there are multiple devices supporting MHI. Client
- *                   should specify 0 for the first device 1 for second etc.
- *  @info:           Client provided callbacks which MHI will invoke on events
- *  @user_data:      Client provided context to be returned to client upon
- *                   callback invocation.
- *  Not thread safe, caller must ensure concurrency protection.
- *
- * @Return MHI_STATUS
- */
-enum MHI_STATUS mhi_register_channel(struct mhi_client_handle **client_handle,
-		enum MHI_CLIENT_CHANNEL chan, s32 device_index,
-		struct mhi_client_info_t *client_info, void *user_data);
-
-/**
- * mhi_open_channel - Client must call this function to open a channel
- *
- * @client_handle:  Handle populated by MHI, opaque to client
- *
- *  Not thread safe, caller must ensure concurrency protection.
- *
- * @Return MHI_STATUS
- */
-enum MHI_STATUS mhi_open_channel(struct mhi_client_handle *client_handle);
-
-/**
- * mhi_queue_xfer - Client called function to add a buffer to MHI channel
- *
- *  @client_handle  Pointer to client handle previously obtained from
- *                  mhi_open_channel
- *  @buf            Pointer to client buffer
- *  @buf_len        Length of the client buffer
- *  @chain          Specify whether to set the chain bit on this buffer
- *  @eob            Specify whether this buffer should trigger EOB interrupt
- *
- *  Not thread safe, caller must ensure concurrency protection.
- *
- * @Return MHI_STATUS
- */
-enum MHI_STATUS mhi_queue_xfer(struct mhi_client_handle *client_handle,
-		dma_addr_t buf, size_t buf_len, enum MHI_FLAGS flags);
-
-/**
- * mhi_close_channel - Client can request channel to be closed and handle freed
- *
- *  @client_handle  Pointer to client handle previously obtained from
- *                  mhi_open_channel
- *  Not thread safe, caller must ensure concurrency protection.
- *
- * @client_handle  Pointer to handle to be released
- */
-void mhi_close_channel(struct mhi_client_handle *client_handle);
-
-/**
- * mhi_get_free_desc - Get the number of free descriptors on channel.
- *  client_handle  Pointer to client handle previously obtained from
- *                      mhi_open_channel.
- *
- * This API returns a snapshot of available descriptors on the given
- * channel
- *
- * @Return  non negative on success
- */
-int mhi_get_free_desc(struct mhi_client_handle *client_handle);
-
-/*
- * mhi_poll_inbound - Poll a buffer from MHI channel
- * @client_handle  Pointer to client handle previously obtained from
- *                      mhi_open_channel.
- * @result         Result structure to be populated with buffer info
- *			if available;
- *
- * Client may asynchronously poll on an inbound channel for descriptors
- * which have been populated. This API is used by client to receive data
- * from device after a callback notification has been received.
- *
- *  Not thread safe, caller must ensure concurrency protection.
- *
- * @Return  non negative on success
- */
-int mhi_poll_inbound(struct mhi_client_handle *client_handle,
-			     struct mhi_result *result);
-
-/**
- * mhi_get_max_desc - Get the maximum number of descriptors
- *			supported on the channel.
- * @client_handle  Pointer to client handle previously obtained from
- *                      mhi_open_channel.
- * @Return  non negative on success
- */
-int mhi_get_max_desc(struct mhi_client_handle *client_handle);
-
-/* RmNET Reserved APIs, This APIs are reserved for use by the linux network
-* stack only. Use by other clients will introduce system wide issues
-*/
-int mhi_set_lpm(struct mhi_client_handle *client_handle, int enable_lpm);
-int mhi_get_epid(struct mhi_client_handle *mhi_handle);
-struct mhi_result *mhi_poll(struct mhi_client_handle *client_handle);
-void mhi_mask_irq(struct mhi_client_handle *client_handle);
-void mhi_unmask_irq(struct mhi_client_handle *client_handle);
 #endif
