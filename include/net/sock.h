3170c24f3ebea57bb1858d7b294d7bf8f43bf7f8
Ben Hutchings <ben@decadent.org.uk>
2017-03-03 02:32:07 +0000

    net/sock: Add sock_efree() function
    Extracted from commit 62bccb8cdb69 ("net-timestamp: Make the clone operation
    stand-alone from phy timestamping").
    
    Change-Id: Iba928f7960373e948bb7ed20751a3ad104832d9d
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1527,6 +1527,7 @@ extern struct sk_buff		*sock_rmalloc(struct sock *sk,
 					      gfp_t priority);
 extern void			sock_wfree(struct sk_buff *skb);
 extern void			sock_rfree(struct sk_buff *skb);
+extern void			sock_efree(struct sk_buff *skb);
 extern void			sock_edemux(struct sk_buff *skb);
 
 extern int			sock_setsockopt(struct socket *sock, int level,
