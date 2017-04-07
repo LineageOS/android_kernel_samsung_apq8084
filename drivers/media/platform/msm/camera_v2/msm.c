32058e42ed76dfe22fa189e720a5620b3bd7bd43
Trishansh Bhardwaj <tbhardwa@codeaurora.org>
2017-04-07 11:16:29 +0530

    msm: camera: Allow driver file to be opend only once.
    Use proper synchronization to ensure driver file is opened
    only once.
    
    CRs-Fixed: 2023513
    Bug: 62378684
    Change-Id: I71e55e2d487fe561d3f596590b3e8102c5e921b5
    Signed-off-by: Trishansh Bhardwaj <tbhardwa@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/msm.c
+++ drivers/media/platform/msm/camera_v2/msm.c
@@ -858,11 +858,8 @@ static int msm_open(struct file *filep)
 	BUG_ON(!pvdev);
 
 	/* !!! only ONE open is allowed !!! */
-	if (atomic_read(&pvdev->opened)) {
-		pr_err("__dbg: Already device node is in opened state \n");
+	if (atomic_cmpxchg(&pvdev->opened, 0, 1))
 		return -EBUSY;
-	}
-	atomic_set(&pvdev->opened, 1);
 
 	spin_lock_irqsave(&msm_pid_lock, flags);
 	msm_pid = get_pid(task_pid(current));
