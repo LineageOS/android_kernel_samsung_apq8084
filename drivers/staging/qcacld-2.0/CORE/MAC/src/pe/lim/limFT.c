44bbf8704763ebca40be2f7f4a1d13abbaa3da0c
Padma, Santhosh Kumar <skpadma@codeaurora.org>
2017-05-23 13:09:28 +0530

    qcacld-2.0: Update dot11mode properly for roamable AP
    prima to qcacld-2.0 propagation
    
    Currently dot11mode is not updated properly for roamable AP. This can
    result in incorrect data rates. Update dot11mode to fix this.
    
    Change-Id: I644f685fc2c0e041b308017ed3206d56c9c57fe9
    CRs-Fixed: 2047070

--- drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limFT.c
+++ drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limFT.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2015 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2017 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -822,7 +822,6 @@ void limFillFTSession(tpAniSirGlobal pMac,
    tPowerdBm         localPowerConstraint;
    tPowerdBm         regMax;
    tSchBeaconStruct  *pBeaconStruct;
-   tANI_U32          selfDot11Mode;
    ePhyChanBondState cbEnabledMode;
 
    pBeaconStruct = vos_mem_malloc(sizeof(tSchBeaconStruct));
@@ -866,8 +865,10 @@ void limFillFTSession(tpAniSirGlobal pMac,
    vos_mem_copy(pftSessionEntry->ssId.ssId, pBeaconStruct->ssId.ssId,
          pftSessionEntry->ssId.length);
 
-   wlan_cfgGetInt(pMac, WNI_CFG_DOT11_MODE, &selfDot11Mode);
-   pftSessionEntry->dot11mode = selfDot11Mode;
+
+   pftSessionEntry->dot11mode =
+                  psessionEntry->ftPEContext.pFTPreAuthReq->dot11mode;
+   limLog(pMac, LOG1, FL("dot11mode %d"), pftSessionEntry->dot11mode);
    pftSessionEntry->vhtCapability =
          (IS_DOT11_MODE_VHT(pftSessionEntry->dot11mode)
          && IS_BSS_VHT_CAPABLE(pBeaconStruct->VHTCaps));
