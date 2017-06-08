4a396e13d7555357946e25bea5c29db428bbd428
Agrawal Ashish <ashishka@codeaurora.org>
2017-06-08 16:30:49 +0530

    qcacld-2.0: TDLS power save check should be done after removing command
    Sme en-queues tdls del sta command to active command queue and releases
    this command only after receiving tdls del sta response.
    On receiving tdls del sta response command, sme calls
    hdd callback (which in turn calls wlan_hdd_check_bmps())
    and remove the del sta command from sme active command queue
    So power save check always fails as del sta command will be
    always pending in sme active command queue.
    
    Fix this by doing power save check once command has been released.
    
    Change-Id: I3e2c4e43afab614f7331b86c4de4306e9698b0bb
    CRs-Fixed: 2058349

--- drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_assoc.c
+++ drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_assoc.c
@@ -2902,7 +2902,6 @@ eHalStatus hdd_RoamTdlsStatusUpdateHandler(hdd_adapter_t *pAdapter,
                     pHddCtx->tdlsConnInfo[staIdx].sessionId = 255;
                     vos_mem_zero(&pHddCtx->tdlsConnInfo[staIdx].peerMac,
                                                sizeof(v_MACADDR_t)) ;
-                    wlan_hdd_tdls_check_bmps(pAdapter);
                     status = eHAL_STATUS_SUCCESS ;
                     break ;
                 }
@@ -2910,6 +2909,12 @@ eHalStatus hdd_RoamTdlsStatusUpdateHandler(hdd_adapter_t *pAdapter,
             complete(&pAdapter->tdls_del_station_comp);
         }
         break ;
+        case  eCSR_ROAM_TDLS_CHECK_BMPS:
+        {
+            wlan_hdd_tdls_check_bmps(pAdapter);
+            status = eHAL_STATUS_SUCCESS ;
+            break;
+        }
         case eCSR_ROAM_RESULT_TEARDOWN_TDLS_PEER_IND:
         {
             VOS_TRACE( VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
@@ -3512,6 +3517,7 @@ hdd_smeRoamCallback(void *pContext, tCsrRoamInfo *pRoamInfo, tANI_U32 roamId,
         case eCSR_ROAM_RESULT_MGMT_TX_COMPLETE_IND:
             wlan_hdd_tdls_mgmt_completion_callback(pAdapter, pRoamInfo->reasonCode);
             break;
+
 #endif
 #ifdef WLAN_FEATURE_11W
        case eCSR_ROAM_UNPROT_MGMT_FRAME_IND:
