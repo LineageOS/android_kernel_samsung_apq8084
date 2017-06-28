7bd87488f27d67eabf53eed9db40469018968354
Corinna Vinschen <xda@vinschen.de>
2017-06-28 19:24:37 +0200

    apq8084: msm: mdss: Fix argument checking vulnerability
    This is apparently a security-related fix.
    
    So far, load_tuning_file is left without fixing up the fs value in
    the error case.  The fs value determines whether argument validity
    checking should be performed or not, and checking is switched off
    on entry.  So leaving the function without restoring fs results
    in missing argument validity checking in subsequent kernel calls
    from user space in this thread.  This patch fixes that problem.
    
    The patch was taken from the klte kernel update to G900FXXU1CQE1
    and adapted to the different panel driver files in the apq8084
    kernel.
    
    Unfortunately, Samsung kernels come without git log messages.
    Neither searching codeaurora, nor cve.mitre.org revealed any matching
    entry.
    
    Change-Id: Iede2efc41b5759a98d2b9250c4c0053625cf458c
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- drivers/video/msm/mdss/samsung/ss_dsi_panel_common.c
+++ drivers/video/msm/mdss/samsung/ss_dsi_panel_common.c
@@ -2809,7 +2809,7 @@ static void load_tuning_file(struct device *dev, char *filename)
 	filp = filp_open(filename, O_RDONLY, 0);
 	if (IS_ERR(filp)) {
 		printk(KERN_ERR "%s File open failed\n", __func__);
-		return;
+		goto err;
 	}
 
 	l = filp->f_path.dentry->d_inode->i_size;
@@ -2819,7 +2819,7 @@ static void load_tuning_file(struct device *dev, char *filename)
 	if (dp == NULL) {
 		pr_info("Can't not alloc memory for tuning file load\n");
 		filp_close(filp, current->files);
-		return;
+		goto err;
 	}
 	pos = 0;
 	memset(dp, 0, l);
@@ -2832,7 +2832,7 @@ static void load_tuning_file(struct device *dev, char *filename)
 		pr_info("vfs_read() filed ret : %d\n", ret);
 		kfree(dp);
 		filp_close(filp, current->files);
-		return;
+		goto err;
 	}
 
 	filp_close(filp, current->files);
@@ -2842,6 +2842,10 @@ static void load_tuning_file(struct device *dev, char *filename)
 	sending_tune_cmd(dev, dp, l);
 
 	kfree(dp);
+
+	return;
+err:
+	set_fs(fs);
 }
 
 static ssize_t tuning_show(struct device *dev,
