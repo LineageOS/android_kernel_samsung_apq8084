116d90ce9a4f832ba7e7b0c69926ea7aa1bb34ce
Maggie White <maggiewhite@google.com>
2017-07-05 13:00:40 -0700

    msm: camera: isp: fix for out of bound access array
    There is no bound check in stream_cfg_cmd->num_streams and it's used in
    several places as a maximum index into the stream_cfg_cmd->stream_handle
    array which has a size of 15. Current code didn't check the maximum
    index to make sure it didn't exceed the array size.
    
    Bug: 62379525
    Change-Id: Idcf639486d235551882dafc34d9e798d78c70bf0
    Signed-off-by: Maggie White <maggiewhite@google.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_stats_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_stats_util.c
@@ -518,6 +518,12 @@ static int msm_isp_start_stats_stream(struct vfe_device *vfe_dev,
 if (rc < 0)
 		return rc;
 
+	if (stream_cfg_cmd->num_streams > MSM_ISP_STATS_MAX) {
+		pr_err("%s invalid num_streams %d\n", __func__,
+			stream_cfg_cmd->num_streams);
+		return -EINVAL;
+	}
+
 	for (i = 0; i < stream_cfg_cmd->num_streams; i++) {
 		idx = STATS_IDX(stream_cfg_cmd->stream_handle[i]);
 
@@ -589,6 +595,12 @@ static int msm_isp_stop_stats_stream(struct vfe_device *vfe_dev,
 
 	num_stats_comp_mask =
 		vfe_dev->hw_info->stats_hw_info->num_stats_comp_mask;
+
+	if (stream_cfg_cmd->num_streams > MSM_ISP_STATS_MAX) {
+		pr_err("%s invalid num_streams %d\n", __func__,
+			stream_cfg_cmd->num_streams);
+		return -EINVAL;
+	}
 
 	for (i = 0; i < stream_cfg_cmd->num_streams; i++) {
 		idx = STATS_IDX(stream_cfg_cmd->stream_handle[i]);
