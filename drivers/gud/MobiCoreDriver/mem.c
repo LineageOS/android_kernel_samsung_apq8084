b7ead6f225c876fe7ee05971ce7daa9c5f021583
Corinna Vinschen <xda@vinschen.de>
2017-07-09 19:51:59 +0200

    apq8084: drop unused FOLL_CMA flag
    commit 71cefecbcd1109e6349f574abfc09b2d9dd15388 removed the experimental
    CONFIG_CMA_PINPAGE_MIGRATION code, but neglected to remove the FOLL_CMA
    flag and the only call using this flag
    
    Change-Id: Ib57be6102f55b0c87b5fb4b85789822a895b921f
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- drivers/gud/MobiCoreDriver/mem.c
+++ drivers/gud/MobiCoreDriver/mem.c
@@ -72,14 +72,14 @@ static int lock_pages(struct task_struct *task, void *virt_start_page_addr,
 
 	/* lock user pages, must hold the mmap_sem to do this. */
 	down_read(&(task->mm->mmap_sem));
-	locked_pages = __get_user_pages(
+	locked_pages = get_user_pages(
 				task,
 				task->mm,
 				(unsigned long)virt_start_page_addr,
 				pages_no,
-				FOLL_TOUCH | FOLL_GET | FOLL_WRITE | FOLL_CMA,
+				1, /* write access */
+				0,
 				pages,
-				NULL,
 				NULL);
 	up_read(&(task->mm->mmap_sem));
 
