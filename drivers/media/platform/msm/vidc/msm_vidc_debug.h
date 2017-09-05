f7dbcb326c85bcbed8cad08f408d07f801a64cd6
Santhosh Behara <santhoshbehara@codeaurora.org>
2017-09-05 06:55:47 -0700

    msm: vidc: Squash the below changes
    [apq8084 NOTE: The first patch, "msm: vidc: Protect debug_buffer..."
     has been already applied before]
    
    msm: vidc: Protect debug_buffer access in core_info_read with lock.
    
    Serialize core_info_read with lock so that multiple concurrent
    threads do not cause the write to overflow. Also have the bound
    check to avoid overflow in write_str function.
    
    Change-Id: Ia18a4b94cafd69af1d367861f2499fc202f18e9f
    
    msm: vidc: Allocate memory dynamically for debugfs info_reads
    
    Use dynamically allocated memory for constructing strings in
    core_info_read & inst_info_read. This ensures that there is no
    contention for a shared memory & hence avoids the requirement of
    a lock. Allocate on demand, as the calls implement a debugfs
    facility and hence rarely invoked. Statically allocated memory
    otherwise remain idle.
    
    Change-Id: Ia18a4b94cafd69af1d367861f2499fc202f18e9f
    
    msm: vidc: Use seq_file for debugfs interface
    
    Print debug logs using seq_file, avoiding intermediate memory.
    
    Bug: 35138888
    
    Change-Id: Ia18a4b94cafd69af1d367861f2499fc202f18e9e
    Signed-off-by: Abdulla Anam <abdullahanam@codeaurora.org>
    Signed-off-by: Sanjay Singh <sisanj@codeaurora.org>
    Signed-off-by: Surajit Podder <spodder@codeaurora.org>
    Signed-off-by: Santhosh Behara <santhoshbehara@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_debug.h
+++ drivers/media/platform/msm/vidc/msm_vidc_debug.h
@@ -109,6 +109,7 @@ extern u32 msm_vidc_firmware_unload_delay;
 
 
 struct dentry *msm_vidc_debugfs_init_drv(void);
+void msm_vidc_debugfs_deinit_drv(void);
 struct dentry *msm_vidc_debugfs_init_core(struct msm_vidc_core *core,
 		struct dentry *parent);
 struct dentry *msm_vidc_debugfs_init_inst(struct msm_vidc_inst *inst,
