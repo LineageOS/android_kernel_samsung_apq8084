c3e38884d691fbe4c2206a5308790ff95e815391
Corinna Vinschen <xda@vinschen.de>
2018-02-15 19:04:39 +0100

    apq8084: drop CONFIG_SEC_OOM_KILLER
    Now that lowmemorykiller is in the same state as bullhead, we can
    drop CONFIG_SEC_OOM_KILLER eventually.
    
    Change-Id: I6bd2b71d2023aba66ae30d5b99a0005be9cf9923
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2553,9 +2553,6 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 	bool sync_migration = false;
 	bool deferred_compaction = false;
 	bool contended_compaction = false;
-#ifdef CONFIG_SEC_OOM_KILLER
-	unsigned long oom_invoke_timeout = jiffies + HZ/50;
-#endif
 
 	/*
 	 * In the slowpath, we sanity check order to avoid ever trying to
@@ -2676,13 +2673,7 @@ rebalance:
 	 * If we failed to make any progress reclaiming, then we are
 	 * running out of options and have to consider going OOM
 	 */
-#ifdef CONFIG_SEC_OOM_KILLER
-#define SHOULD_CONSIDER_OOM (!did_some_progress \
-		|| time_after(jiffies, oom_invoke_timeout)) && boot_mode != 1
-#else
-#define SHOULD_CONSIDER_OOM !did_some_progress && boot_mode != 1
-#endif
-	if (SHOULD_CONSIDER_OOM) {
+	if (!did_some_progress) {
 		if ((gfp_mask & __GFP_FS) && !(gfp_mask & __GFP_NORETRY)) {
 			if (oom_killer_disabled)
 				goto nopage;
@@ -2690,12 +2681,6 @@ rebalance:
 			if ((current->flags & PF_DUMPCORE) &&
 			    !(gfp_mask & __GFP_NOFAIL))
 				goto nopage;
-#ifdef CONFIG_SEC_OOM_KILLER
-			if (did_some_progress)
-				pr_info("time's up : calling "
-					"__alloc_pages_may_oom(o:%d, gfp:0x%x)\n", order, gfp_mask);
-
-#endif
 
 			page = __alloc_pages_may_oom(gfp_mask, order,
 					zonelist, high_zoneidx,
