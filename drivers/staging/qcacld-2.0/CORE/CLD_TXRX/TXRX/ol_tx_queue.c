ea4ad71c25905ee77dab648e346a5dca483597d4
Corinna Vinschen <xda@vinschen.de>
2018-02-15 13:53:59 +0100

    Revert "qcacld-2.0: Add argument to ol_tx_queue_free for vdev or peer queues"
    This reverts commit 7dea9473917552322a40ab0e20694fccb8bab12a.
    
    Change-Id: I2775c5c789062917685ad78b62a532d89924d607

--- drivers/staging/qcacld-2.0/CORE/CLD_TXRX/TXRX/ol_tx_queue.c
+++ drivers/staging/qcacld-2.0/CORE/CLD_TXRX/TXRX/ol_tx_queue.c
@@ -67,7 +67,7 @@ void
 ol_tx_queue_log_free(
     struct ol_txrx_pdev_t *pdev,
     struct ol_tx_frms_queue_t *txq,
-    int tid, int frms, int bytes, bool is_peer_txq);
+    int tid, int frms, int bytes);
 #define OL_TX_QUEUE_LOG_ENQUEUE ol_tx_queue_log_enqueue
 #define OL_TX_QUEUE_LOG_DEQUEUE ol_tx_queue_log_dequeue
 #define OL_TX_QUEUE_LOG_FREE    ol_tx_queue_log_free
@@ -76,8 +76,7 @@ ol_tx_queue_log_free(
 
 #define OL_TX_QUEUE_LOG_ENQUEUE(pdev, msdu_info, frms, bytes) /* no-op */
 #define OL_TX_QUEUE_LOG_DEQUEUE(pdev, txq, frms, bytes) /* no-op */
-/* no-op */
-#define OL_TX_QUEUE_LOG_FREE(pdev, txq, tid, frms, bytes, is_peer_txq)
+#define OL_TX_QUEUE_LOG_FREE(pdev, txq, tid, frms, bytes) /* no-op */
 
 #endif /* TXRX_DEBUG_LEVEL > 5 */
 
@@ -114,12 +113,11 @@ ol_tx_queue_vdev_flush(struct ol_txrx_pdev_t *pdev, struct ol_txrx_vdev_t *vdev)
         * into scheduler, so use same tid when we flush them
         */
         if (i == OL_TX_VDEV_MCAST_BCAST)
-            ol_tx_queue_free(pdev, txq, HTT_TX_EXT_TID_NON_QOS_MCAST_BCAST,
-                             false);
+            ol_tx_queue_free(pdev, txq, HTT_TX_EXT_TID_NON_QOS_MCAST_BCAST);
         else if (i == OL_TX_VDEV_DEFAULT_MGMT)
-            ol_tx_queue_free(pdev, txq, HTT_TX_EXT_TID_MGMT, false);
+            ol_tx_queue_free(pdev, txq, HTT_TX_EXT_TID_MGMT);
         else
-            ol_tx_queue_free(pdev, txq, (i + OL_TX_NUM_TIDS), false);
+            ol_tx_queue_free(pdev, txq, (i + OL_TX_NUM_TIDS));
     }
     /* flush PEER TX queues */
     do {
@@ -145,7 +143,7 @@ ol_tx_queue_vdev_flush(struct ol_txrx_pdev_t *pdev, struct ol_txrx_vdev_t *vdev)
             for (j = 0; j < OL_TX_NUM_TIDS; j++) {
                 txq = &peers[i]->txqs[j];
                 if (txq->frms) {
-                    ol_tx_queue_free(pdev, txq, j, true);
+                    ol_tx_queue_free(pdev, txq, j);
                 }
             }
             TXRX_PRINT(TXRX_PRINT_LEVEL_ERR,
@@ -317,7 +315,7 @@ void
 ol_tx_queue_free(
     struct ol_txrx_pdev_t *pdev,
     struct ol_tx_frms_queue_t *txq,
-    int tid, bool is_peer_txq)
+    int tid)
 {
     int frms = 0, bytes = 0;
     struct ol_tx_desc_t *tx_desc;
@@ -340,9 +338,9 @@ ol_tx_queue_free(
         txq->frms--;
         tx_desc = TAILQ_NEXT(tx_desc, tx_desc_list_elem);
     }
-    OL_TX_QUEUE_LOG_FREE(pdev, txq, tid, frms, bytes, is_peer_txq);
+    OL_TX_QUEUE_LOG_FREE(pdev, txq, tid, frms, bytes);
     txq->bytes -= bytes;
-    OL_TX_QUEUE_LOG_FREE(pdev, txq, tid, frms, bytes, is_peer_txq);
+    OL_TX_QUEUE_LOG_FREE(pdev, txq, tid, frms, bytes);
     txq->flag = ol_tx_queue_empty;
     /* txq->head gets reset during the TAILQ_CONCAT call */
     TAILQ_CONCAT(&tx_tmp_list, &txq->head, tx_desc_list_elem);
@@ -1168,7 +1166,7 @@ void
 ol_tx_queue_log_free(
     struct ol_txrx_pdev_t *pdev,
     struct ol_tx_frms_queue_t *txq,
-    int tid, int frms, int bytes, bool is_peer_txq)
+    int tid, int frms, int bytes)
 {
     u_int16_t peer_id;
     struct ol_tx_log_queue_add_t *log_elem;
@@ -1178,7 +1176,7 @@ ol_tx_queue_log_free(
         return;
     }
 
-    if ((tid < OL_TX_NUM_TIDS) && is_peer_txq) {
+    if (tid < OL_TX_NUM_TIDS) {
         struct ol_txrx_peer_t *peer;
         struct ol_tx_frms_queue_t *txq_base;
 
