ab754909473470a08d7664f92cd65b0f767f900d
Stefan Assmann <sassmann@kpanic.de>
2018-07-27 17:42:28 +0200

    apq8084: remove #define DEBUG for mdnie_lite
    Change-Id: Ie65c7c4c398f78c089cd804ebc56cd299a9faad6

--- drivers/video/msm/mdss/samsung/ss_dsi_mdnie_lite_common.c
+++ drivers/video/msm/mdss/samsung/ss_dsi_mdnie_lite_common.c
@@ -31,7 +31,7 @@ Copyright (C) 2012, Samsung Electronics. All rights reserved.
 */
 #include "ss_dsi_mdnie_lite_common.h"
 
-#define MDNIE_LITE_TUN_DEBUG
+/* #define MDNIE_LITE_TUN_DEBUG */
 
 #ifdef MDNIE_LITE_TUN_DEBUG
 #define DPRINT(x...)	printk(KERN_ERR "[mdnie_lite] " x)
