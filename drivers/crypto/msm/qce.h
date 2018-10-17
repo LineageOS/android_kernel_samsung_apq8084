3ddcb390f1c19291c0241f16a5a952c8f1e4e830
Corinna Vinschen <xda@vinschen.de>
2018-10-17 17:47:56 +0200

    Revert "crypto: msm: Add aead hmac-sha256 support"
    This reverts commit 162014a882369151f7be1696281b06b74192d38d.
    
    Change-Id: Ibadff4448605852431fc5b20b5fad873e230e648

--- drivers/crypto/msm/qce.h
+++ drivers/crypto/msm/qce.h
@@ -149,8 +149,7 @@ struct qce_req {
 	void *areq;
 	enum qce_cipher_alg_enum   alg;	/* cipher algorithms*/
 	enum qce_cipher_dir_enum dir;	/* encryption? decryption? */
-	enum qce_cipher_mode_enum mode;	/* algorithm mode  */
-	enum qce_hash_alg_enum auth_alg;/* authentication algorithm for aead */
+	enum qce_cipher_mode_enum mode;	/* algorithm mode */
 	unsigned char *authkey;		/* authentication key  */
 	unsigned int authklen;		/* authentication key kength */
 	unsigned int authsize;		/* authentication key kength */
