cc034889e467203ccd5365ae77dc5382294b57a0
Stefan Assmann <sassmann@kpanic.de>
2019-02-14 23:14:33 +0100

    qcacld-2.0: disable link layer stats
    Link layer stats triggers firmware timeout.
    02-17 21:28:32.793   390   390 E WifiHAL : wifi_get_link_stats: requestResponse Error:-7
    02-17 21:28:32.805   567  1525 E WifiVendorHal: getWifiLinkLayerStats(l.937) failed {.code = ERROR_UNKNOWN, .description = , timed out}
    They weren't queried before pie, so deal without them.
    
    Unfortunately I didn't find a way to teach wifi HAL not to query this so
    the log still gets spammed with
    02-19 11:31:10.316   393   393 E WifiHAL : wifi_get_link_stats: requestResponse Error:-3
    02-19 11:31:10.320   568  1615 E WifiVendorHal: getWifiLinkLayerStats(l.937) failed {.code = ERROR_NOT_SUPPORTED, .description = }
    every few seconds. But at least it doesn't hang the device anymore.
    
    Change-Id: Ia00d19b7384605c6ee6b7b68d441a4616e9816a6

--- drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_cfg80211.c
+++ drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -5826,7 +5826,7 @@ qca_wlan_vendor_ll_get_policy[QCA_WLAN_VENDOR_ATTR_LL_STATS_GET_MAX +1] =
     [QCA_WLAN_VENDOR_ATTR_LL_STATS_GET_CONFIG_REQ_MASK] = { .type = NLA_U32 }
 };
 
-static int __wlan_hdd_cfg80211_ll_stats_get(struct wiphy *wiphy,
+static int __maybe_unused __wlan_hdd_cfg80211_ll_stats_get(struct wiphy *wiphy,
                                           struct wireless_dev *wdev,
                                           const void *data,
                                           int data_len)
@@ -5931,7 +5931,7 @@ static int __wlan_hdd_cfg80211_ll_stats_get(struct wiphy *wiphy,
  *
  * Return: 0 if success, non-zero for failure
  */
-static int wlan_hdd_cfg80211_ll_stats_get(struct wiphy *wiphy,
+static int __maybe_unused wlan_hdd_cfg80211_ll_stats_get(struct wiphy *wiphy,
 					struct wireless_dev *wdev,
 					const void *data,
 					int data_len)
@@ -7939,6 +7939,7 @@ const struct wiphy_vendor_command hdd_wiphy_vendor_commands[] =
         .doit = (void *)wlan_hdd_cfg80211_ll_stats_set
     },
 
+#if 0 /* times out firmware, lags device */
     {
         .info.vendor_id = QCA_NL80211_VENDOR_ID,
         .info.subcmd = QCA_NL80211_VENDOR_SUBCMD_LL_STATS_GET,
@@ -7947,6 +7948,7 @@ const struct wiphy_vendor_command hdd_wiphy_vendor_commands[] =
                  WIPHY_VENDOR_CMD_NEED_RUNNING,
         .doit = (void *)wlan_hdd_cfg80211_ll_stats_get
     },
+#endif
 #endif /* WLAN_FEATURE_LINK_LAYER_STATS */
 /* EXT TDLS */
     {
