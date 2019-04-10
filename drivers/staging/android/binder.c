4be593d3564b86c985431cadd2bc3e0dd838e05d
Corinna Vinschen <xda@vinschen.de>
2019-04-10 12:33:27 +0200

    apq8084: binder: fix an accidentally moved line in a former patch
    commit 3f8eac284c20a97c0ad355fc3daf482bd674cf1c moved an
    INIT_LIST_HEAD into a wrong spot.
    
    Change-Id: I59c226a2ef47c3c0440c373baffc50bc7ba91a8e
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -3521,10 +3521,10 @@ static int binder_open(struct inode *nodp, struct file *filp)
 	INIT_LIST_HEAD(&proc->todo);
 	init_waitqueue_head(&proc->wait);
 	proc->default_priority = task_nice(current);
-	INIT_LIST_HEAD(&proc->buffers);
 	binder_dev = container_of(filp->private_data, struct binder_device,
 				  miscdev);
 	proc->context = &binder_dev->context;
+	INIT_LIST_HEAD(&proc->buffers);
 
 	binder_lock(__func__);
 
