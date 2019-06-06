1f64f17451ac8b19e4e339ebb7066dbd6385501f
Pragaspathi Thilagaraj <tpragasp@codeaurora.org>
2019-06-06 11:10:37 +0200

    qcacld-2.0: Fix possible buffer overflow in sirConvertAddtsReq2Struct
    [CV: backported from qcacld-3.0, fixes CVE-2019-2269]
    
    In the function sir_convert_addts_rsp2_struct, iterator j is
    assigned with the value pAddTs->numTclas + addts.num_WMMTCLAS.
    The j value is used as the index to the array pAddTs->tclasInfo.
    Maximum limit on  pAddTs->tclasInfo entries is 2. So when the
    value of j exceeds 2, then a possible buffer overflow could
    occur.
    
    Validate the value of j against SIR_MAC_TCLASIE_MAXNUM(2).
    
    Change-Id: Icc723380ed4ccd51c729194d509e288be0e0712c
    CRs-Fixed: 2264429

--- drivers/staging/qcacld-2.0/CORE/SYS/legacy/src/utils/src/parserApi.c
+++ drivers/staging/qcacld-2.0/CORE/SYS/legacy/src/utils/src/parserApi.c
@@ -4032,7 +4032,7 @@ sirConvertAddtsReq2Struct(tpAniSirGlobal    pMac,
         if ( addts.num_WMMTCLAS )
         {
             j = (tANI_U8)(pAddTs->numTclas + addts.num_WMMTCLAS);
-            if ( SIR_MAC_TCLASIE_MAXNUM > j ) j = SIR_MAC_TCLASIE_MAXNUM;
+            if ( SIR_MAC_TCLASIE_MAXNUM < j ) j = SIR_MAC_TCLASIE_MAXNUM;
 
             for ( i = pAddTs->numTclas; i < j; ++i )
             {
@@ -4214,7 +4214,7 @@ sirConvertAddtsRsp2Struct(tpAniSirGlobal    pMac,
         if ( addts.num_WMMTCLAS )
         {
             j = (tANI_U8)(pAddTs->numTclas + addts.num_WMMTCLAS);
-            if ( SIR_MAC_TCLASIE_MAXNUM > j ) j = SIR_MAC_TCLASIE_MAXNUM;
+            if ( SIR_MAC_TCLASIE_MAXNUM < j ) j = SIR_MAC_TCLASIE_MAXNUM;
 
             for ( i = pAddTs->numTclas; i < j; ++i )
             {
